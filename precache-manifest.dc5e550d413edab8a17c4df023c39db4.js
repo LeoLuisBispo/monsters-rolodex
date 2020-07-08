f0c842b67ccbea76c9a36d86e29988b6e255d902
LeoLuisBispo <l.luisbispo@outlook.com>
2020-07-08 16:14:46 +0200

    Updates

---
+++ precache-manifest.dc5e550d413edab8a17c4df023c39db4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c659615d618fa22bb0afb1ad5b0c631",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9f655f3f462ab006499b",
+    "url": "/monsters-rolodex/static/css/main.fb95d717.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f655f3f462ab006499b",
+    "url": "/monsters-rolodex/static/js/main.8bd1e99c.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
